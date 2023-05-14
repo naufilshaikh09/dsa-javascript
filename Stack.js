cec6f7096c2b5ccab14304b352833c679307ba3a
Naufil Shaikh <snaufil09@gmail.com>
2023-05-14 11:43:25 +0530

    Stack

---
+++ Stack.js
@@ -0,0 +1,43 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.top = newNode;
+        this.length = 1;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.top = newNode;
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
+        }
+        this.length++;
+    }
+
+    pop() {
+        if (this.length === 0) return undefined;
+
+        let temp = this.top;
+        this.top = this.top.next;
+        temp.next = null;
+
+        this.length--;
+        return temp;
+    }
+}
+
+let myStack = new Stack(1);
+myStack.push(2);
+myStack.push(3);
+myStack.push(4);
+myStack.pop();
+console.log(myStack);
